5e33e46787bc4d46a1a177029e126009f4df9921
ajit <ajitagustiani10@gmail.com>
2019-10-28 21:08:33 -0700

    config body perser

--- server.js
+++ server.js
@@ -1,51 +1,17 @@
 const express = require('express')
 const app =express()
 const port = 3000
-const bodyParser = require("body-parser")
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-// //instal app
-// bikin folder di dalam folder
-// npm init --yes
-// npm i express --save
-// install nodemon
-// npm instal -g nodemon
-app.get('/',(req, res) => res.send('Hello word-'))
-//run aplikasi 
-// node server.js / nodemon server.js
-// aplikasi bisa dibuka dengan localhost:3000
-const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/belajarmongo')//,{useNewUrlParser : true});
-//bikin model untuk collection person
-const PersonModel = mongoose.model("person",{
-    firstname: String,
-    lastname: String 
-});
+//memanggil libary body parser
+const bodyParser = require('body-parser');
+//config body parser
+app.use(bodyParser.urlencoded({extended:true})); //menangkap type request dalam bentuk form urlencoded
 
-app.post('/create',async (req, res)=>{
-    //Do something here
-    console.log(req.body)
-    var person = new PersonModel(req.body);
-    var result = await person.save();
-    const response = {
-        statusCode : 200,
-        error : "",
-        message : "create Person",
-        content : result
-    }
-    res.json(response);
-})
-app.get('/list', async (req, res) =>{
-    var person = await PersonModel.find().exec();
-    const response = {
-        statusCode : 200,
-        error : '',
-        message : "List Person",
-        content : person
-    }
-    res.json(response)
-})
-app.get('/hello', function(req,res){
+app.use(bodyParser.json());//menangkap url dalam bentuk json
+
+app.get('/',(req, res) => res.send('Hello word-'))
+
+//membuat request post
+app.post('/hello', function(req,res){
     const respon = {
         statusCode : 200,
         error : "",
@@ -53,14 +19,6 @@ app.get('/hello', function(req,res){
     }
     res.json(respon);
 })
-app.post('/Profile' , function(req, res){
-    console.log(req.body)
-    const respon = {
-        statusCode : 200,
-        error : "",
-        content : req.body
-    }
-    res.json(respon);
-})
+//commit lagi dengan nama "membuat requst post"
 app.listen(port, () => console.log(`Example app listening on port $(port)!`))
 
